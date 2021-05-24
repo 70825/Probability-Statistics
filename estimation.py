809a6054b2fc13b3ba8ff68a1582ca4fe4e97f20
Dabeen Jeong <hello70825@gmail.com>
2021-05-24 10:32:27 +0900

    update

--- estimation.py
+++ estimation.py
@@ -1,19 +1,20 @@
 import numpy as np
 np.random.seed(0)
 
+
 class sampling:
-    def __str__(self):
-        print('Randomly sample {} samples from population : {}'.format(self.n, ', '.join(map(str, self.s))))
-        print()
-        print(f'Estimate sample mean: {self.mean}')
-        print(f'Estimate sample variance: {self.var}')
-
-        return ''
-
     def __init__(self, data, n):
         self.n = n
         self.s = np.random.choice(data, n)
         self.mean, self.var = self.get_mean_and_variance()
+
+    def __str__(self):
+        print('Randomly sample {} samples from population : {}'.format(self.n, ', '.join(map(str, self.s))))
+        print()
+        print(f'Estimate mean in sample distribution: {self.mean}')
+        print(f'Estimate variance in sample distribution: {self.var}')
+
+        return ''
 
     def get_mean_and_variance(self):
         def get_mean():
@@ -29,10 +30,12 @@ class sampling:
 
         return [self.mean, self.var]
 
-    def estimate_population_mean_and_variance(self):
+    def estimate_population_mean_and_variance(self, population_var):
         def estimate_population_mean():
-            min_val = self.mean - 1.96 * self.var / np.sqrt(self.n)
-            max_val = self.mean + 1.96 * self.var / np.sqrt(self.n)
+            StDev = np.sqrt(population_var)
+
+            min_val = self.mean - 1.96 * StDev / np.sqrt(self.n)
+            max_val = self.mean + 1.96 * StDev / np.sqrt(self.n)
 
             min_val = np.round_(min_val, 2)
             max_val = np.round_(max_val, 2)
@@ -46,20 +49,19 @@ class sampling:
             max_val = np.round_(max_val, 2)
             return min_val, max_val
 
-        chi = {}
-        chi[10] = [19.023, 2.7]
-        chi[30] = [45.722, 16.047]
-        chi[61] = [83.298, 40.482]
+        chi = {10: [19.023, 2.7], 30: [45.722, 16.047], 61: [83.298, 40.482]}
 
         epm = estimate_population_mean()
-        print(f'Estimate population mean with 95% confidence interval : {epm[0]} < μ < {epm[1]}')
+        print(f'Using sample distribution,Estimate population mean with 95% confidence interval : {epm[0]} < μ < {epm[1]}')
 
         epv = estimate_population_var()
-        print(f'Estimate population variance with 95% confidence interval : {epv[0]} < σ^2 < {epv[1]}')
+        print(f'Using sample distribution, Estimate population variance with 95% confidence interval : {epv[0]} < σ^2 < {epv[1]}')
 
     def estimate_population_mean_t_distribution(self):
-        min_val = self.mean - 2.447 * self.var / np.sqrt(self.n)
-        max_val = self.mean + 2.447 * self.var / np.sqrt(self.n)
+        StDev = np.sqrt(self.var)
+
+        min_val = self.mean - 2.447 * StDev / np.sqrt(self.n)
+        max_val = self.mean + 2.447 * StDev / np.sqrt(self.n)
 
         min_val = np.round_(min_val, 2)
         max_val = np.round_(max_val, 2)
